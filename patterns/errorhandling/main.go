4db31227e2311a1b75155c6cad9768e1c323a6bb
c10t <c10t@fa.ke>
2018-11-10 23:47:32 +0900

    a bit fix for breaking

--- patterns/errorhandling/main.go
+++ patterns/errorhandling/main.go
@@ -35,10 +35,16 @@ func main() {
 	done := make(chan interface{})
 	defer close(done)
 
-	urls := []string{"https://www.google.com", "https://badhost"}
+	errCount := 0
+	urls := []string{"a", "https://www.google.com", "b", "c", "d"}
 	for result := range checkStatus(done, urls...) {
 		if result.Error != nil {
-			fmt.Printf("error: %v", result.Error)
+			fmt.Printf("error: %v\n", result.Error)
+			errCount++
+			if errCount >= 3 {
+				fmt.Println("Too many errors, breaking!")
+				break
+			}
 			continue
 		}
 
