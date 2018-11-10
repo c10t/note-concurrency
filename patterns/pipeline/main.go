9d51422c7bf4f8a7f0afd19f50e293d0330ba47f
c10t <c10t@fa.ke>
2018-11-11 00:45:25 +0900

    change from batch style to stream style

--- patterns/pipeline/main.go
+++ patterns/pipeline/main.go
@@ -4,25 +4,17 @@ import (
 	"fmt"
 )
 
-func multiply(values []int, multiplier int) []int {
-	multipliedValues := make([]int, len(values))
-	for i, v := range values {
-		multipliedValues[i] = v * multiplier
-	}
-	return multipliedValues
+func multiply(value, multiplier int) int {
+	return value * multiplier
 }
 
-func add(values []int, additive int) []int {
-	addedValues := make([]int, len(values))
-	for i, v := range values {
-		addedValues[i] = v + additive
-	}
-	return addedValues
+func add(value, additive int) int {
+	return value + additive
 }
 
 func main() {
 	ints := []int{1, 2, 3, 4}
-	for _, v := range multiply(add(multiply(ints, 2), 1), 2) {
-		fmt.Println(v)
+	for _, v := range ints {
+		fmt.Println(multiply(add(multiply(v, 2), 1), 2))
 	}
 }
