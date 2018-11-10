5904d4e1d563e2bc5fb5cd50c40840eb7f1ead6c
c10t <c10t@fa.ke>
2018-11-11 04:45:15 +0900

    change to use channel

--- patterns/pipeline/main.go
+++ patterns/pipeline/main.go
@@ -4,17 +4,73 @@ import (
 	"fmt"
 )
 
-func multiply(value, multiplier int) int {
-	return value * multiplier
+func generator(done <-chan interface{}, integers ...int) <-chan int {
+	intStream := make(chan int, len(integers))
+
+	go func() {
+		defer close(intStream)
+		for _, i := range integers {
+			select {
+			case <-done:
+				return
+			case intStream <- i:
+			}
+		}
+	}()
+
+	return intStream
 }
 
-func add(value, additive int) int {
-	return value + additive
+func multiply(
+	done <-chan interface{},
+	intStream <-chan int,
+	multiplier int,
+) <-chan int {
+	multipliedStream := make(chan int)
+
+	go func() {
+		defer close(multipliedStream)
+		for i := range intStream {
+			select {
+			case <-done:
+				return
+			case multipliedStream <- i * multiplier:
+			}
+		}
+	}()
+
+	return multipliedStream
+}
+
+func add(
+	done <-chan interface{},
+	intStream <-chan int,
+	additive int,
+) <-chan int {
+	addedStream := make(chan int)
+
+	go func() {
+		defer close(addedStream)
+		for i := range intStream {
+			select {
+			case <-done:
+				return
+			case addedStream <- i + additive:
+			}
+		}
+	}()
+
+	return addedStream
 }
 
 func main() {
-	ints := []int{1, 2, 3, 4}
-	for _, v := range ints {
-		fmt.Println(multiply(add(multiply(v, 2), 1), 2))
+	done := make(chan interface{})
+	defer close(done)
+
+	intStream := generator(done, 1, 2, 3, 4)
+	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+
+	for v := range pipeline {
+		fmt.Println(v)
 	}
 }
