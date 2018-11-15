dbb2bb71e06f596fc5d4986e772ace387dc8df41
c10t <c10t@fa.ke>
2018-11-15 09:59:36 +0900

    create bad test

---
+++ patterns/heartbeat/doworker_test.go
@@ -0,0 +1,26 @@
+package doworker
+
+import (
+	"testing"
+  "time"
+)
+
+func TestDoWork_GeneratesAllNumbers(t *testing.T) {
+	done := make(chan interface{})
+	defer close(done)
+
+	intSlice := []int{0, 1, 2, 3, 5}
+	_, results := DoWork(done, intSlice...)
+
+	for i, expected := range intSlice {
+		select {
+		case r := <-results:
+			if r != expected {
+				t.Errorf("index %v: expected %v, but received %v", i, expected, r)
+			}
+		case <-time.After(1 * time.Second):
+			t.Fatal("test timed out")
+		}
+	}
+
+}
